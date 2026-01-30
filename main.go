1e87fd254de41c172431a44f5d00ef879625e435
cp-endor <chandra@endor.ai>
2026-01-30 10:54:32 +0530

    branch1commit3

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/dgrijalva/jwt-go"
 	_ "github.com/gogo/protobuf/proto"
 	_ "github.com/hashicorp/golang-lru"
-	// _ "github.com/owncast/owncast/logging"
+	_ "github.com/owncast/owncast/logging"
 )
 
 func main() {
