60aa36ab34e229a7ead6e1b47ddc69e83328ddd9
cp-endor <chandra@endor.ai>
2026-01-30 10:53:30 +0530

    branch1commit2

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gogo/protobuf v1.3.2
+	github.com/hashicorp/golang-lru v1.0.2
 )
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	_ "github.com/dgrijalva/jwt-go"
 	_ "github.com/gogo/protobuf/proto"
-	// _ "github.com/hashicorp/golang-lru"
+	_ "github.com/hashicorp/golang-lru"
 	// _ "github.com/owncast/owncast/logging"
 )
 
